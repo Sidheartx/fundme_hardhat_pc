25d46e7abdf1d59909dff9cdf40c03189b97d0c6
Sid <Siddharth.dny@gmail.com>
2023-08-10 18:00:03 +0530

    contracts

--- contracts/PriceConvertor.sol
+++ contracts/PriceConvertor.sol
@@ -1,55 +1,30 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-/// contract is supposed to accept ETH funding by anyone and then let the owner withdraw
-pragma solidity 0.8.19; 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-/// gas efficiency 
-contract FundMe {
+library PriceConverter {
+  function getPrice(AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    (, int256 answer, , , ) = priceFeed.latestRoundData();
+    // ETH/USD rate in 18 digit
+    return uint256(answer * 10000000000);
+  }
 
-//// constant read-only calls are cheaper than non-constant variables
-//// immutable variables are also lower gas cost 
-    uint256 public constant min_ETH = 0.01 * 10 ** 18; 
-    address _owner; 
-    constructor() {
-
-        _owner = msg.sender; 
-    }
-
-    mapping(address => uint256) public funderValue;
-    address[] public funders;  
-    event funded(address funder, uint _value); 
-    event fundWithdrawn(uint _amount); 
-    event ownerchanged(address _newOwner); 
-
-    function fund() public payable {
-        require(msg.value >= min_ETH);
-        funderValue[msg.sender] = msg.value; 
-        funders.push(msg.sender); 
-        emit funded(msg.sender, msg.value);
-    }
-
-    modifier onlyOwner{
-        require(msg.sender==_owner,"not the owner");
-        _;
-    }
-
-    function withdraw() public onlyOwner {
-        require (address(this).balance > min_ETH); 
-         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        emit fundWithdrawn(address(this).balance); 
-         _setFunderstoZero(); 
-    } 
-
-    function changeOwnerToBub(address _newOwner) public onlyOwner{
-        _owner = _newOwner;  
-        emit ownerchanged(_newOwner);
-    }
-    
-    function _setFunderstoZero() public {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex + 1) {
-            address funder = funders[funderIndex]; 
-            funderValue[funder] = 0; 
-        } 
-    } 
-
+  // 1000000000
+  // call it get fiatConversionRate, since it assumes something about decimals
+  // It wouldn't work for every aggregator
+  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    uint256 ethPrice = getPrice(priceFeed);
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+    // the actual ETH/USD conversation rate, after adjusting the extra 0s.
+    return ethAmountInUsd;
+  }
 }
